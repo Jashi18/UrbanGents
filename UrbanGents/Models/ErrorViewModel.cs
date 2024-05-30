7cd7001c59074f80e5d7ad485149e1c993af8291
User <User@DESKTOP-IC46P8B>
2024-05-30 12:05:36 +0400

    Add project files.

---
+++ UrbanGents/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace UrbanGents.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
